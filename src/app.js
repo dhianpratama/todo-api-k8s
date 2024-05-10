9d786cabdba85f616284b60d1901e7959112a27a
Dhian <muthidp@gmail.com>
2024-05-10 18:38:39 +1000

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./config/config.js");
+const todoRoutes = require("./routes/todo.routes.js");
+
+// create express app
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("error", function (err) {
+  console.log(`Could not connect to the database: ${err}`);
+  process.exit();
+});
+
+mongoose.connection.once("open", function () {
+  console.log("Successfully connected to the database");
+});
+
+// Todo Routes
+app.use("/api/todos", todoRoutes);
+
+// listen for requests
+const PORT = config.port;
+app.listen(PORT, function () {
+  console.log(`Server is listening on port ${PORT}`);
+});
